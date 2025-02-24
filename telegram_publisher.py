e3db061e1863abbaf83e5a7cd04c03971c656017
Tikhovskoy <tihovskoyviktor@icloud.com>
2025-02-24 17:10:02 +0700

    Update telegram_publisher.py

--- telegram_publisher.py
+++ telegram_publisher.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-CHANNEL_ID = "-1002460010506"  # Правильный `chat_id`
+CHANNEL_ID = "-1002460010506"
 
 bot = telegram.Bot(token=BOT_TOKEN)
 bot.send_message(chat_id=CHANNEL_ID, text="Бот успешно отправил сообщение в канал!")
