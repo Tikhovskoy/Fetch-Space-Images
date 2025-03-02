654884b43ad8a88fb6eb70311ba0c2c3d5704a10
Tikhovskoy <tihovskoyviktor@icloud.com>
2025-03-02 09:04:50 +0700

    Update fetch_spacex_images.py
    Добавил возможность передавать путь к папке через --directory или переменную окружения

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 from image_downloader import download_all_images
 
@@ -6,7 +7,11 @@ def main():
     Основная функция для загрузки изображений SpaceX.
     Запрашивает изображения последнего запуска и сохраняет их в указанную папку.
     """
-    download_all_images(directory="images")
+    parser = argparse.ArgumentParser(description="Скачивание изображений SpaceX.")
+    parser.add_argument("--directory", type=str, default=os.getenv("IMAGES_DIR", "images"), help="Папка для сохранения изображений")
+    args = parser.parse_args()
+
+    download_all_images(directory=args.directory)
 
 if __name__ == "__main__":
     main()
