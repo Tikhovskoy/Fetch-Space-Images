2d83abe750099c1118fad326a6bdec0dabfa856c
Tikhovskoy <tihovskoyviktor@icloud.com>
2025-03-02 09:11:54 +0700

    Update fetch_nasa_epic.py
    Добавил возможность менять путь к папке через аргументы  или переменную окружения

--- fetch_nasa_epic.py
+++ fetch_nasa_epic.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 from image_downloader import download_all_images
 
@@ -11,10 +12,11 @@ def main():
     Функция вызывает `download_all_images()`, передавая папку сохранения и количество изображений.
     """
     parser = argparse.ArgumentParser(description="Скачивание изображений NASA EPIC.")
+    parser.add_argument("--directory", type=str, default=os.getenv("IMAGES_DIR", "images"), help="Папка для сохранения изображений")
     parser.add_argument("--count", type=int, default=10, help="Количество изображений")
     args = parser.parse_args()
 
-    download_all_images(directory="images", count=args.count)
+    download_all_images(directory=args.directory, count=args.count)
 
 if __name__ == "__main__":
     main()
